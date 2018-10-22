4e22d6f945a48beb826947d5bdbf96a377d1ceb1
Gregory Servino <40346841+Gregoryserv@users.noreply.github.com>
2018-10-21 22:13:13 -0300

    Add files via upload

--- alterar-funcionario.php
+++ alterar-funcionario.php
@@ -3,24 +3,22 @@ session_start();
 ob_start();
 include_once 'util/helper.class.php';
 
-if(isset($_GET['id'])){
+if (isset($_GET['id'])) {
   include_once "dao/funcionariodao.class.php";
   include_once "modelo/funcionario.class.php";
 
-  $funDAO = new FuncionarioDAO();
-  $array = $funDAO->filtrar($_GET['id'], "codigo");
+  $funcionarioDAO = new FuncionarioDAO();
+  $array = $funcionarioDAO->filtrar($_GET['id'], "codigo");
 
 
-
-  $fun = $array[0];
-
+  $funcionario = $array[0];
 
 }
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-  <title>Edição dos Funcionarios</title>
+  <title>Cadastro de Funcionário</title>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
   <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
@@ -30,37 +28,28 @@ if(isset($_GET['id'])){
 </head>
   <body>
       <div class="container">
-        <h1 class="jumbotron bg-info">Edição dos Funcionarios</h1>
+        <h1 class="jumbotron bg-info">Cadastro de Funcionário</h1>
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Sistema</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link" href="index.php">Home</a>
-              </li>
-
-              <li class="nav-item">
-                <a class="nav-link" href="cadastro-carro.php">Cad. Carros <span class="sr-only">(current)</span></a>
+            <li class="nav-item active">
+                <a class="nav-link" href="cadastro-pet.php">Cadastro de Pet</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="consulta-carros.php">Cons. Carros</a>
+                <a class="nav-link" href="consulta-pets.php">Consulta de Pets</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="cadastro-cliente.php">Cad. Clientes <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="cadastro-cliente.php">Cadastro de Cliente</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="consulta-clientes.php">Cons. Clientes</a>
+                <a class="nav-link" href="consulta-clientes.php">Consulta de Clientes</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="cadastro-funcionario.php">Cad. Funcionarios <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="cadastro-funcionario.php">Cadastro de Funcionário</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="consulta-funcionarios.php">Cons. Funcionarios</a>
+                <a class="nav-link" href="consulta-funcionarios.php">Consulta de Clientes</a>
               </li>
             </ul>
           </div>
@@ -71,64 +60,45 @@ if(isset($_GET['id'])){
         ?>
         <form name="cadfuncionario" method="post" action="">
           <div class="form-group">
-            <input type="text" name="txtnome" placeholder="Nome" class="form-control"
-                   value="<?php if(isset($fun)){ echo $fun->nome; }?>">
+            <input type="text" name="txtnome" placeholder="Nome do funcionário" class="form-control" value="<?php if(isset($funcionario)){ echo $funcionario->nome; }?>">
           </div>
           <div class="form-group">
-            <input type="text" name="txtcpf" placeholder="CPF" class="form-control"
-                   value="<?php if(isset($fun)){ echo $fun->cpf; }?>">
+            <input type="text" name="txtendereco" placeholder="Endereco" class="form-control" value="<?php if(isset($funcionario)){ echo $funcionario->endereco; }?>">
           </div>
           <div class="form-group">
-            <input type="text" name="txtendereco" placeholder="Endereco" class="form-control"
-                   value="<?php if(isset($fun)){ echo $fun->endereco; }?>">
+            <input type="text" name="txtfuncao" placeholder="Complemento" class="form-control" value="<?php if(isset($funcionario)){ echo $funcionario->funcao; }?>">
           </div>
           <div class="form-group">
-            <input type="number" name="txtsalario" placeholder="Salario" class="form-control"
-            value="<?php if(isset($fun)){ echo $fun->salario; }?>">
+            <input type="number" name="txtcpf" placeholder="CPF" class="form-control" value="<?php if(isset($funcionario)){ echo $funcionario->cpf; }?>">
           </div>
           <div class="form-group">
-            <select name="selfuncao" class="form-control">
-              <option value="Vendedor" <?php
-                                     if(isset($fun)){
-                                        if($fun->funcao == "Vendedor"){
-                                          echo "selected='selected'";
-                                        }
-                                     }
-                                     ?>
-                                     >Vendedor</option>
-              <option value="Gerente" <?php if(isset($fun)){if($fun->funcao == "Gerente"){echo "selected='selected'";}} ?>>Gerente</option>
-              <option value="Faxineiro" <?php if(isset($fun)){if($fun->funcao == "Faxineiro"){echo "selected='selected'";}} ?>>Faxineiro</option>
-            </select>
+            <input type="number" name="txtsalario" placeholder="Salário" class="form-control" value="<?php if(isset($funcionario)){ echo $funcionario->salario; }?>">
           </div>
           <div class="form-group">
-            <input type="submit" name="alterar" value="Alterar" class="btn btn-primary">
+            <input type="submit" name="Alterar" value="Alterar" class="btn btn-primary">
             <input type="reset" name="Limpar" value="Limpar" class="btn btn-danger">
           </div>
         </form>
         <?php
+        if(isset($_POST['Alterar'])){
+          include_once 'modelo/funcionario.class.php';
+          include_once 'dao/funcionariodao.class.php';
+          include 'util/padronizacao.class.php';
 
-          if(isset($_POST['alterar'])){
-            include_once 'modelo/funcionario.class.php';
-            include_once 'dao/funcionariodao.class.php';
-            include 'util/padronizacao.class.php';
+          $funcionario = new Funcionario();
+          $funcionario->id = $_GET['id'];
+          $funcionario->nome = Padronizacao::antiXSS(Padronizacao::padronizarMaiMin($_POST['txtnome']));
+          $funcionario->endereco = Padronizacao::antiXSS(Padronizacao::padronizarMaiMin($_POST['txtendereco']));
+          $funcionario->funcao = Padronizacao::antiXSS(Padronizacao::padronizarMai($_POST['txtfuncao']));
+          $funcionario->cpf = Padronizacao::antiXSS($_POST['txtcpf']);
+          $funcionario->salario = Padronizacao::antiXSS($_POST['txtsalario']);
 
-            $fun = new Funcionario();
-            $fun->idFuncionario= $_GET['id'];
-            $fun->nome = Padronizacao::antiXSS(Padronizacao::padronizarMaiMin($_POST['txtnome']));
-            $fun->cpF = Padronizacao::antiXSS(Padronizacao::padronizarMaiMin($_POST['txtcpf']));
-            $fun->endereco = Padronizacao::antiXSS(Padronizacao::padronizarMaiMin($_POST['txtendereco']));
-            $fun->salario = $_POST['txtsalario'];
-            $fun->funcao = Padronizacao::antiXSS($_POST['selfuncao']);
-            $funDAO = new FuncionarioDAO();
-            $funDAO->alterarFuncionario($fun);
+          $funcionarioDAO = new FuncionarioDAO();
+          $funcionarioDAO->cadastrarFuncionario($funcionario);
 
-            $_SESSION['msg'] = "Funcionario alterado com sucesso!";
-            header("location:consulta-funcionarios.php");
-
-            //echo "<h2>Funcionarios cadastrado com sucesso!</h2>";
-            //Helper::alert("Funcionario cadastrado com sucesso!");
-            //echo $fun;
-            ob_end_flush();
+          $_SESSION['msg'] = "Funcionário cadastrado com sucesso!";
+          header("location:consulta-funcionarios.php");
+          ob_end_flush();
           }
         ?>
       </div>
